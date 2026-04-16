d1f7822f136dbf34a427c0f48f1174fe049a6b28
antonifarinango <farinangoanthony1@gmail.com>
2026-04-15 21:22:16 -0500

    cookies

--- src/api/api.js
+++ src/api/api.js
@@ -7,17 +7,38 @@ const api = axios.create({
   xsrfHeaderName: 'X-XSRF-TOKEN',
 });
 
+let capturedCsrfToken = null;
+
 api.interceptors.request.use((config) => {
-  const csrfToken = document.cookie
+  // Try to get from document.cookie first
+  const cookieToken = document.cookie
     .split("; ")
     .find(c => c.startsWith("XSRF-TOKEN"))
     ?.split("=")[1];
+
+  const csrfToken = cookieToken || capturedCsrfToken;
 
   if (csrfToken) {
     config.headers["X-XSRF-TOKEN"] = csrfToken;
   }
 
   return config;
+});
+
+api.interceptors.response.use((response) => {
+  // Capture token from response headers (works even if cross-domain)
+  // Axios normalizes headers to lowercase, but we can check both just in case
+  const token = response.headers['x-xsrf-token'] || response.headers['X-XSRF-TOKEN'];
+  if (token) {
+    capturedCsrfToken = token;
+  }
+  return response;
+}, (error) => {
+  const token = error.response?.headers['x-xsrf-token'] || error.response?.headers['X-XSRF-TOKEN'];
+  if (token) {
+    capturedCsrfToken = token;
+  }
+  return Promise.reject(error);
 });
 
 export default api;
